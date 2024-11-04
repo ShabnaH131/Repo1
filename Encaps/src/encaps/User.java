5bcc2af4c27fa595f240b9b75ac69b80636e3774
ShabnaH131 <shabnah5252@gmail.com>
2024-11-04 14:40:13 +0530

    Final Commit

---
+++ Encaps/src/encaps/User.java
@@ -0,0 +1,15 @@
+package encaps;
+
+public class User {
+
+	public static void main(String[] args) {
+		Test ob=new Test();
+		ob.setName("Shabna");
+		ob.setAge(2);
+		
+		System.out.println(ob.getName());
+		System.out.println(ob.getAge());
+
+	}
+
+}
